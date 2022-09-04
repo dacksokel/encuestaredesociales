3cee4f03429525c9d003e8854e8bea7d96de2289
Jhony M. Navarro M <dacksokel@gmail.com>
2022-09-04 13:59:44 -0400

    update establecida la conexion con la base de dato

--- database.js
+++ database.js
@@ -3,23 +3,21 @@ require("dotenv").config({
   path: "t.env",
 });
 //para el servidor online
-// mongoose.connect(process.env.DB_URI,{
-//         //solucion al error que daba al conectar la base de datos
-//         useNewUrlParser: true,
-//         useFindAndModify: false,
-//         useCreateIndex: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(db => console.log('base de dato conectado a mongocloud'))
-//     .catch(err => console.error("problemas al conectar la base de datos"))
+mongoose.connect(process.env.DB_URI,{
+        //solucion al error que daba al conectar la base de datos
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(db => console.log('base de dato conectado a mongocloud'))
+    .catch(err => console.error("problemas al conectar la base de datos", err))
 
-const URI = "mongodb://localhost:27017/encuestas";
-mongoose
-  .connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((db) => console.log("base de dato conectada correctamente"))
-  .catch((err) => console.log("error al conectar a la base de dato", err));
+// const URI = "mongodb://localhost:27017/encuestas";
+// mongoose
+//   .connect(URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((db) => console.log("base de dato conectada correctamente"))
+//   .catch((err) => console.log("error al conectar a la base de dato", err));
 
 module.exports = mongoose;
